bb868acdfd1988a6170ba190dd8040f823d05baa
DIvkov575 <Dmitriy@Ivkov.net>
2022-08-26 17:05:01 -0400

    save

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-# Scrollable 3D Animation with Three.js
+https://github.com/mrdoob/three.js/blob/9ef27d1af7809fa4d9943f8d4c4644e365ab6d2d/examples/webgl_geometries.html#L144
 
-- Watch the [full tutorial](https://youtu.be/Q7AOvWpIVHU) on YouTube
-- [Scrollable Three.js Animation](https://fireship.io/snippets/threejs-scrollbar-animation) Snippet
+https://discourse.threejs.org/t/animate-multiple-objects-simultaneously-in-a-scene/19880/5
 
-## Usage
+https://threejs.org/docs/#manual/en/introduction/Animation-system
 
-```
-git clone <this-repo>
-npm install
-npm run dev
-```
+
+
+update cam fov
+camera.fov = 20;
+camera.updateProjectionMatrix();
+
+1) create asteroid feild
+2) finish buttons
+3) change block width + opacity
+4) changes according to screen size !!
--- main.js
+++ main.js
@@ -1,70 +1,99 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { randRange, randRotation } from './components';
 
-// Setup
-
+let cubeList = [];
 const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
-});
-
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#bg'),});
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 camera.position.setX(-3);
 renderer.render(scene, camera);
 
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
 
-
-
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  // const geometry = new THREE.BoxGeometry(0.25, 24, 24);
-
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const star = new THREE.Mesh(geometry, material);
-
-// edges geometry
-  // const geometry = new THREE.EdgesGeometry( mesh.geometry ); // or WireframeGeometry
-  // const material = new THREE.LineBasicMaterial( { color: 0xffff00, linewidth: 2 } );
-  // const star = new THREE.LineSegments( geometry, material );
-  // mesh.add( edges );
-
+//  cubes !!
+function addWireFrameCube() {
+  const cubeGeometry = new THREE.BoxGeometry(5,5,5);
+  const cubeGeo = new THREE.EdgesGeometry( cubeGeometry ); // or WireframeGeometry( geometry )
+  const mat = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 2 } );
+  const wireframe = new THREE.LineSegments( cubeGeo, mat );
   const [x, y, z] = Array(3)
     .fill()
     .map(() => THREE.MathUtils.randFloatSpread(100));
-
-  star.position.set(x, y, z);
-  scene.add(star);
+  wireframe.position.set(x, y, z);
+  wireframe.rotation.x = randRange(0,360);
+  wireframe.rotation.y = randRange(0,360);
+  wireframe.rotation.z = randRange(0,360);
+  scene.add(wireframe);
+  return wireframe;
 }
-Array(100).fill().forEach(addStar);
+function addBigWireFrameCube() {
+  const cubeBigGeometry = new THREE.BoxGeometry(15,15,15);
+  const cubeBigGeo = new THREE.EdgesGeometry( cubeBigGeometry ); // or WireframeGeometry( geometry )
+  const mat = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 2 } );
+  const wireframe = new THREE.LineSegments( cubeBigGeo, mat );
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+  wireframe.position.set(x, y, z);
+  wireframe.rotation.x = randRange(0,360);
+  wireframe.rotation.y = randRange(0,360);
+  wireframe.rotation.z = randRange(0,360);
+  scene.add(wireframe);
+  return wireframe;
+}
+for (let index = 0; index < 75; index++) {
+  cubeList.push(addWireFrameCube())
+}
+for (let index = 0; index < 3; index++) {
+  cubeList.push(addBigWireFrameCube())
+}
 
-// Background
-scene.background = "black";
 
+// asteroids !!
+function addAsteroid() {
+  const sphereGeo = new THREE.SphereGeometry(5,20,20);
+  const material = new THREE.LineBasicMaterial( {color: 0xff1111});
+  const asteroid = new THREE.LineSegments
+}
+
+
+
+// Animation !!
+let rate = 1
 function moveCamera() {
-  const t = document.body.getBoundingClientRect().top;
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.rotation.y = t * -0.0002;
+  const t = -document.body.getBoundingClientRect().top;
+  if (t < 1200) {
+    camera.position.z = t * 0.1;
+    camera.position.x = t * 0.001;
+    camera.rotation.y = t * 0.0002;
+    camera.rotation.x = t * 0.0005;
+    for (let i = 0; i < cubeList.length; i++){randRotation(cubeList[i], t)}
+  } 
+  else if (t > 1300 && t < 1400) {
+  camera.rotation.x = 0;
+  camera.rotation.y = 1;  
+  camera.rotation.z = 0;
 }
+  else if (t > 900 && t < 1350) {
+    console.log(t)
+    camera.rotation.y -= (0-camera.rotation.y) / rate*7
+    camera.rotation.z -= (0-camera.rotation.x) / rate*7
+    rate += (t-1350) * 0.2
+  }
 
-document.body.onscroll = moveCamera;
-moveCamera();
-
-// Animation Loop
-
+  else if (t >1800) {
+  }
+}
 function animate() {
-
+  requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
-
+document.body.onscroll = moveCamera;
+moveCamera();
 animate();
+
+// Standard JS
