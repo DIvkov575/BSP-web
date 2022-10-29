a968840873100fdb04220601ac2c83720bdff7d6
DIvkov575 <Dmitriy@Ivkov.net>
2022-10-29 17:37:21 -0400

    save before switching branches

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ server.get('/', function(req, res) {
 
 server.get('/a', function(req, res) {
     res.sendFile(__dirname + '/public/about.html');
-    // res.send("alskfhjd")
-    // res.sendFile(__dirname + 'public/main.js');
+});
+
+server.get('/sign-up', function(req, res) {
+    res.sendFile(__dirname + '/public/sign-up.html');
 });
 
 server.listen(3000, () => console.log("Server Listening on Port 3000"))
