5ee5107f1a5b3983dd2686a1b84bbd93a926e32f
DIvkov575 <Dmitriy@Ivkov.net>
2022-12-11 14:38:35 -0500

    save

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ server.get('/', function(req, res) {
     // res.sendFile(__dirname + 'public/main.js');
 });
 
-server.get('/a', function(req, res) {
+server.get('/about', function(req, res) {
     res.sendFile(__dirname + '/public/about.html');
 });
 
 server.get('/sign-up', function(req, res) {
     res.sendFile(__dirname + '/public/sign-up.html');
+});
+
+server.get('/questions', function(req, res) {
+    res.sendFile(__dirname + '/public/question-form.html');
 });
 
 server.listen(3000, () => console.log("Server Listening on Port 3000"))
