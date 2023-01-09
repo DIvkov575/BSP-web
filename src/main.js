0d13872080b5d883a0ba40215595f01e9ed88b39
DIvkov575 <Dmitriy@Ivkov.net>
2023-01-09 17:06:51 -0500

    v2.o.1

--- src/main.js
+++ src/main.js
@@ -75,10 +75,10 @@ function moveCamera() {
   camera.position.z = (-t * 0.025) + 100;
 
   if (t < 2500) {
-  gridEdge.rotation.y += 0.5
+  gridEdge.rotation.y += 0.4
   }
   if (t > 2500) {
-    gridEdge.rotation.y += 0.5
+    gridEdge.rotation.y += 0.4
     camera.rotation.y = -(t-2500)*0.00015;
     camera.position.x = (t-2500)*0.00015;
   }
