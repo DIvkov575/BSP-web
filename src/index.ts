74860de5323240e7cb695f4ab108c86ebb43a16f
Divkov575 <dmitriy@ivkov.net>
2023-06-04 20:43:19 -0400

    OOP three js !!!!!!!

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-// import {animate, moveCamera} from "./three-stuff.js";
-// let three = require("./three-stuff.js");
+// import {animatera} from "./three-stuff.js";
 import "./three-stuff.js";
 import "./main.scss";
 import "./assets/dmitriyI.png";
 import "./assets/favicon.svg";
-console.log("TS connected");
+let three = require("./three-stuff.js");
 
 
 const elementIsVisible = (el: Element, displacement: number = 0): Boolean => {
@@ -18,25 +17,35 @@ let e1 = document.querySelector("#ChooseUs-scroll-padding");
 let e1_1 = document.querySelector(".ChooseUs");
 let e2 = document.querySelector(".py101");
 let e3 = document.querySelector(".pricing-div");
-// let e4 = document.querySelector(".tri-box-wrapper");
 
-// document.body.onscroll = () => {
-    // three.moveCamera;
-    // if (e1 != null && e1_1 !=null) {
-    //     if (elementIsVisible(e1_1)) {
-    //         e1_1.classList.add("smooth_to_square_0");
-    //     }
-    // }
-    // if (e2 != null) {
-    //     if (elementIsVisible(e2)) {
-    //         e2.classList.add("smooth_to_square_1");
-    //     }
-    // }
+let canv = new three.FullCanvas();
 
-    // if (e3 != null) {
-    //     if (elementIsVisible(e3)) {
-    //         e3.classList.add("smooth_to_square_2");
-    //     }}
-// }
-// three.moveCamera();
-// three.animate();
+window.onscroll = () => {
+    canv.moveCamera();
+    if (e1 != null && e1_1 !=null) {
+        if (elementIsVisible(e1_1)) {
+            e1_1.classList.add("smooth_to_square_0");
+        } else {
+            e1_1.classList.remove("smooth_to_square_0");
+        }
+    }
+    if (e2 != null) {
+        if (elementIsVisible(e2)) {
+            e2.classList.add("smooth_to_square_1");
+        } else {
+            e2.classList.remove("smooth_to_square_1");
+        }
+    }
+
+    if (e3 != null) {
+        if (elementIsVisible(e3)) {
+            e3.classList.add("smooth_to_square_2");
+        } else {
+            e3.classList.remove("smooth_to_square_2");
+        }
+    }
+}
+
+console.log("TS connected");
+canv.moveCamera()
+canv.animate()
